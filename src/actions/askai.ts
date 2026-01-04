160f16de66bde1feb99184b96975df19b0086adb
Avinash Kumar Singh <abhinashsinghwk2@gmail.com>
2026-01-04 19:21:26 +0530

    use @google/genai dependency

--- src/actions/askai.ts
+++ src/actions/askai.ts
@@ -1,18 +1,24 @@
 'use server'
 
-import { GoogleGenerativeAI } from "@google/generative-ai"
+import { GoogleGenAI } from "@google/genai";
 import prisma from '@/lib/db';
+
+const ai = new GoogleGenAI({});
 
 export default async function Askai(question: string, senderId: string) {
     try {
-        const genAI = new GoogleGenerativeAI(process.env.CHAT_API || '');
-        const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-        const result = await model.generateContent(question);
-        await prisma.chat.create({data: {senderId, question, answer: result.response.text()}})
-        return result.response.text()
+
+        const response = await ai.models.generateContent({
+            model: "gemini-2.5-flash",
+            contents: question,
+        });
+
+        const result =  response.text || "Unable to process your request. Try again!"
+        await prisma.chat.create({ data: { senderId, question, answer: result } })
+        return result
     } catch (e: unknown) {
         console.log(e)
         return "Unable to process your request. Try again!"
     }
 
-} 
+}
